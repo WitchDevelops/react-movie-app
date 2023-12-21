ce92f7d5487472394f9585438f59c79391aeb7c9
WitchDevelops <d.n.wojewska@gmail.com>
2023-12-21 11:11:14 +0100

    feat: render a movie card component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useEffect } from 'react';
+import MovieCard from './MovieCard';
 import SearchIcon from './assets/search.svg';
 import './App.css';
 
 // AOI key: 5c560c08
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=5c560c08';
+
+const movie1 = {
+  
+    "Title": "The Lion King",
+    "Year": "1994",
+    "imdbID": "tt0110357",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg"
+
+}
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -22,15 +33,18 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Lion King"
-          onChange={() => {}} // TODO
+          value="The Lion King"
+          onChange={() => { }} // TODO
         >
         </input>
-        <img 
-          src={SearchIcon} 
+        <img
+          src={SearchIcon}
           alt="search"
-          onClick={() => {}} // TODO
+          onClick={() => { }} // TODO
         />
+      </div>
+      <div className="container">
+        <MovieCard movie={movie1}/>
       </div>
     </div>
   )
