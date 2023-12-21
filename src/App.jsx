90efb7c2c0ad1c29aa888a381b34d0508534c6ca
WitchDevelops <d.n.wojewska@gmail.com>
2023-12-21 15:32:40 +0100

    feat: add loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
 import SearchIcon from './assets/search.svg';
+import './assets/LoadingSpinner.css';
 import './App.css';
 
 // AOI key: 5c560c08
-
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=5c560c08';
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=5c560c08";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-    setMovies(data.Search);
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      if (data.Response === 'True') {
+        setMovies(data.Search);
+      } else {
+        setError(data.Error);
+      }
+    } catch (error) {
+      setError('An error occured while fetching data')
+    }
+    setLoading(false);
   }
   useEffect(() => {
     searchMovies('The Lion King');
@@ -43,16 +56,21 @@ const App = () => {
       </div>
 
       {
-        movies?.length > 0 ? (
+        loading ? (
+          <div className="loading-spinner"></div>
+        ) : error ? (
+          <div className="error">{error}</div>
+        ) :
+          movies?.length > 0 ? (
             <>
-            <div>
-              <p>Found {movies.length} movies for your '{searchTerm}' search</p>
-            </div>
-            <div className="container">
-              {movies.map((movie, index) => (
-                <MovieCard movie={movie} key={index}/>
-              ))}
-            </div>
+              <div>
+                <p>Found {movies.length} movies for your '{searchTerm}' search</p>
+              </div>
+              <div className="container">
+                {movies.map((movie, index) => (
+                  <MovieCard movie={movie} key={index} />
+                ))}
+              </div>
             </>
           ) : (
             <div className="empty">
